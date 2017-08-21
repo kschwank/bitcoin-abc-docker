3337515e98f6e09ebc06e59451df88c691d2b83b
Karsten Schwank <karsten.schwank@iteratec.de>
2017-08-21 11:15:28 +0200

    optionally create local volume directory in start script

--- bitcoin-qt-bch.sh
+++ bitcoin-qt-bch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker-xauth
+mkdir -p ${HOME}/.bitcoin-bch
 xauth nlist ${DISPLAY} | sed -e 's/^..../ffff/' | xauth -f ${XAUTH} nmerge -
 docker run -ti --rm -e "XAUTHORITY=${XAUTH}" -v ${HOME}/.bitcoin-bch:/bitcoin -v ${XAUTH}:${XAUTH} -v ${XSOCK}:${XSOCK} --name="bitcoin-qt-bch" kschwank/bitcoin-abc-qt
 
